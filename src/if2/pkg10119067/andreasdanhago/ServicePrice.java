82bc7f352618b243a412ab191fc6df0843897d49
User <User@DESKTOP-6N6EHH6>
2020-11-27 21:25:53 +0700

    PBOIF2-10119067

---
+++ src/if2/pkg10119067/andreasdanhago/ServicePrice.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if2.pkg10119067.andreasdanhago;
+
+/**
+ *
+ * @author User
+ */
+class ServicePrice {
+
+    void displayService() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    String getPriceService() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    boolean checkMemberStatus(String next) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    String getSale(boolean member, String priceService) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    String getTotalPay(String priceService, String sale) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
