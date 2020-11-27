31a53a4b0fd816ca6cab837cd0f891bb4d4b448c
User <User@DESKTOP-6N6EHH6>
2020-11-27 21:26:19 +0700

    PBOIF2-10119067

---
+++ src/if2/pkg10119067/andreasdanhago/CustomerInvoice.java
@@ -0,0 +1,17 @@
+  
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if2.pkg10119067.andreasdanhago;
+
+/**
+ *Nama  : Andreas Danhago P.I
+ *Kelas : PBO2
+ *NIM   : 10119067
+ *Deskripsi Program : Quiz
+ */
+public interface CustomerInvoice {
+    public String currentTime();
+}
